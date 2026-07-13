29368574b781d537b932dd9b1bba1edf8f215840
AkhilKrs221 <rahakrish0@gmail.com>
2026-07-13 23:42:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const officialLinks = {
   notifications: "https://www.keralapsc.gov.in/notifications",
-  schedule: "https://www.keralapsc.gov.in/examination-schedule",
+  schedule: "https://www.keralapsc.gov.in/examinations",
   syllabus: "https://www.keralapsc.gov.in/syllabus1",
   questions: "https://www.keralapsc.gov.in/previous-question-papers",
 };
